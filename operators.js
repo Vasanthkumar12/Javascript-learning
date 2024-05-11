6fc65c06703304a7511412c568ec865c66b260b0
Vasanthkumar M <vasanth23402@gmail.com>
2024-05-11 20:16:44 +0530

    Operators concept in js has completed

---
+++ operators.js
@@ -0,0 +1,20 @@
+let a = 10
+let b = 20
+let c = 30
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+console.log("floor division = " + Math.floor(a,b))
+console.log("a++ = " + a++)
+console.log("++a = " + ++a)
+console.log("a-- = " + a--)
+console.log("--a = " + --a)
+console.log("type = " + typeof(a))
+
+let d = '5'
+console.log("type = " + typeof(d))
+console.log("d==5 " + d == 5)
+console.log("d==5 " + d === 5)
