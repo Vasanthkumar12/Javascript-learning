703777bf94df6aaff136ed593c450e4c4aaff707
Vasanthkumar M <vasanth23402@gmail.com>
2024-05-13 15:47:00 +0530

    Math functions has completed

---
+++ math.js
@@ -0,0 +1,34 @@
+let a = 25
+
+console.log("square root function")
+console.log(Math.sqrt(a))
+
+console.log("random function")
+console.log(Math.random())
+
+console.log("round function")
+console.log(Math.round(5.3))
+console.log(Math.round(5.5))
+console.log(Math.round(5.8))
+
+console.log("floor function")
+console.log(Math.floor(5.3))
+console.log(Math.floor(5.5))
+console.log(Math.floor(5.7))
+
+console.log("ceil function")
+console.log(Math.ceil(5.2))
+console.log(Math.ceil(5.5))
+console.log(Math.ceil(5.7))
+
+console.log("absolute function")
+console.log(Math.abs(5.7))
+
+console.log("pow function")
+console.log(Math.pow(5, 2))
+
+console.log("min function")
+console.log(Math.min(5, 2, 3, 5, 6, 1))
+
+console.log("max function")
+console.log(Math.max(5, 2, 3, 5, 6, 1))
