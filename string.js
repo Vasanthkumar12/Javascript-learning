286c7824899fbaaac4482597b910ebec79d62672
Vasanthkumar M <vasanth23402@gmail.com>
2024-05-13 14:42:36 +0530

    String in JS was completed

---
+++ string.js
@@ -0,0 +1,30 @@
+let str1 = "Hi this is vasanthkumar"
+let str2 = "and my native is tamilnadu."
+
+console.log(str1.length)
+console.log(str1.concat(str2))
+console.log(str1 + str2)
+console.log(str1.concat(" *** ", str2))
+console.log(str1.charAt(5))
+console.log(str1.charCodeAt(5))
+console.log(str1[5])
+console.log(str1[-1])
+console.log(str1.substring(3,10))
+console.log(str1.slice(3,10))
+console.log(str1.toUpperCase)
+console.log(str1.toLowerCase)
+console.log(str1.indexOf("t"))
+console.log(str1.indexOf("this"))
+console.log(str1.lastIndexOf("t"))
+let str3 = "  Hi this is vasanthkumar   "
+console.log(str1.trim)
+console.log("The index of = " + str1.search("is"))
+console.log(str1.includes("this"))
+console.log(str1.includes("this oi"))
+console.log(str1.startsWith("this"))
+console.log(str1.startsWith("Hi"))
+
+
+
+
+
