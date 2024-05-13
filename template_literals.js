481a3e0cefbeed7d49f43612d756b9713b29f02e
Vasanthkumar M <vasanth23402@gmail.com>
2024-05-13 16:18:32 +0530

    Template Literals has completed

---
+++ template_literals.js
@@ -0,0 +1,15 @@
+let firstName = "Vasanth"
+let lastName = "Kumar"
+let village = "Endapatti"
+
+// Normal String concatenation
+console.log(firstName + " " + lastName + " " + "lives in " + village)
+
+// Template Literals
+console.log(`${firstName} ${lastName} lives in ${village}`)
+
+let myName = `Vasanth
+Kumar
+`
+console.log(myName)
+
