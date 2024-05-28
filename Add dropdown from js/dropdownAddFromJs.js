1fd731360ccaacc57235f9e516ff14922447cec0
Vasanthkumar M <vasanth23402@gmail.com>
2024-05-28 17:15:56 +0530

    Add Dropdown contents from JS file

---
+++ Add dropdown from js/dropdownAddFromJs.js
@@ -0,0 +1,11 @@
+// let dropDown = document.getElementById('dropdown')
+const select = document.querySelector('select'); 
+let eee = new Option('EEE','EEE');
+select.add(eee,undefined);
+let ece = new Option('ECE','ECE');
+select.add(ece,undefined);
+let cse = new Option('CSE','CSE');
+select.add(cse,undefined);
+let civil = new Option('CIVIL','CIVIL');
+select.add(civil,undefined);
+
